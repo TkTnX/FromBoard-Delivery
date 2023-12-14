833351933421220320a0e3ea52efe41d3ba2103d
TkTnX <timur20-09@mail.ru>
2023-12-14 22:08:47 +0500

    FromBoard Delivery v2

--- Delivery/js/script.js
+++ Delivery/js/script.js
@@ -1,9 +1,31 @@
-const burger = document.querySelector('.burger-icon')
-const menu = document.querySelector('.menu')
+/* BURGER MENU */
+
+const burger = document.querySelector(".burger-icon");
+const menu = document.querySelector(".menu");
 
 if (burger && menu) {
-    burger.addEventListener('click', () => {
-        burger.classList.toggle('_active')
-        menu.classList.toggle('_active')
-    })
+  burger.addEventListener("click", () => {
+    burger.classList.toggle("_active");
+    menu.classList.toggle("_active");
+  });
 }
+
+/* SWIPER */
+
+const swiper = new Swiper(".feedback-slider", {
+  // Optional parameters
+  slidesPerView: 1,
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: -70,
+    },
+  },
+});
